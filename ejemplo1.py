fd32c714b656ee460c1660452a497a4808e5fd27
andrrsbe <andreselferrari@hotmail.com>
2018-08-23 19:47:02 -0500

    Aumente la resta y la multiplicacion

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,9 +1,12 @@
 #Este es el ejemplo1 de python
 #Programador: Andres
-
 num1 = 20
 num2 = 10
 
 suma = num1 + num2
+resta = num1 - num2
+mult = num1 * num2
 
 print ("El resultado de la suma es: ", suma)
+print ("El resultado de la resta es: ", resta)
+print ("El resultado de la multiplicacion es: ", mult)
