1e49faea3f1721b0342449a7709d29520ce803f1
andrrsbe <andreselferrari@hotmail.com>
2018-08-23 20:02:01 -0500

    ejemplo2Q

---
+++ ejemplo2.py
@@ -0,0 +1,13 @@
+#Este es el ejemplo1 de python
+#Programador: Andres
+num1 = 20
+num2 = 10
+
+resta = num1 - num2
+mult = num1 * num2
+div  =  num1/num2
+
+print ("El resultado de la suma es: ", num1 + num2)
+print ("El resultado de la resta es: ", resta)
+print ("El resultado de la multiplicacion es: ", mult)
+print ("El resultado de la division es: ", div)
